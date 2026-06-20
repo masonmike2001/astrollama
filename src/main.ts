9464092de7a5e1389cafce1e656a877118b7398c
masonmike2001 <masonmike2001@gmail.com>
2026-06-20 13:08:23 -0400

    feat: add wiki creation command, and smart title retrieval

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,37 @@ export default class Astrollama extends Plugin {
 			},
 		});
 
+		this.addCommand({
+			id: 'create-wiki',
+			name: 'Create Wiki',
+			editorCallback: async (editor) => {
+				// wiki template either default or custom fields user will enter in settings
+				const selected = editor.getSelection();
+				const template = "Summary, History, Uses"
+				new Notice("Processing wiki...");
+				const answer = await askOllama("You will create a structured and well formatted wiki article page for an Obsidian note following Obsidian formatting guidelines, including headers starting with # symbols and keeping the title of the entity as the first line. Ensure it follows only this standard layout: " + template +". Use these notes to create the wiki article: " + selected);
+				const firstLine = answer.split('\n')[0];
+
+				const cleaned = firstLine
+				.replace(/^#+\s*/, '')
+				.replace(/[*_`]/g, '')
+				.split(':')[0]
+				.trim();
+
+				editor.replaceSelection(selected + "\n********\n See more: " + "[[Wiki_"+  "" + cleaned + "" + ".md]]");
+				await this.app.vault.create(
+					`Wiki_${cleaned}.md`,
+					answer
+				);
+
+				new Notice("Wiki created at " + `Wiki_${cleaned}.md`);
+
+
+			},
+		});
+
+
+
 // 		// This creates an icon in the left ribbon.
 // 		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
 // 			// Called when the user clicks the icon.
